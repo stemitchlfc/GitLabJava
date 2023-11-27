3b409f3471d3f50738b97e470ae52e3a4281f6c7
Steven Mitchell <stemitchlfc1995@gmail.com>
2023-11-27 21:19:42 +0000

    add dollar a day

---
+++ src/main/java/com/project/DollarADay.java
@@ -0,0 +1,11 @@
+package com.project;
+
+public class DollarADay {
+    public int calculateTotal(int days){
+        int total = 0;
+        for(int i = 1; i<=days; i++){
+            total += i;
+        }
+        return total;
+    }
+}
